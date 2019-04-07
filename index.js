121bfe64e6d0e140fe586b9376048688652feca0
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2019-04-07 10:35:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 #!/usr/bin/env node
 
-const readline = require('readline');
-const cb = require('clipboardy');
-const eol = require('os').EOL;
+const readline = require('readline')
+const cb = require('clipboardy')
+const eol = require('os').EOL
 
-console.reset = function () {
-  return process.stdout.write('\033c');
-}
+console.reset = () => return process.stdout.write('\033c')
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-});
+})
 
 rl.question("What is your Discord Bot Client ID ? ", (res) => {
-  console.reset();
-  const output = `https://discordapp.com/oauth2/authorize?&client_id=${res}&scope=bot`;
-  console.log(`${eol}Your Discord Bot invitation link is : ${output}${eol}`);
-  console.log('This link is now in your clipboard !');
-  cb.writeSync(output);
-  rl.close();
-});
+  console.reset()
+  const output = `https://discordapp.com/oauth2/authorize?&client_id=${res}&scope=bot`
+  console.log(`${eol}Your Discord Bot invitation link is : ${output}${eol}`)
+  console.log('This link is now in your clipboard !')
+  cb.writeSync(output)
+  rl.close()
+})
